44413875a2dfdf7bf5bdcc009505dd082ff6b145
Sheraz Maqsood <sheraz_maqsood@cmhlmc.edu.pk>
2023-07-26 14:41:44 +0500

    changes from old copy

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -34,7 +34,7 @@
     <div class="hero-content">
     <h1 class="mb20 wow fadeInUp" data-wow-delay=".8s"> Design Products<br> Deliver Experience </h1>
     <p class="wow fadeInUp" data-wow-delay="1.2s">A full-service digital marketing firm that specialises in human-centered experiences.</p>
-    <a href="contact-1.html" class="sw-btn sw-blue-btn wow fadeInUp mt40" data-wow-delay="1.4s">Get a quote <i class="fa-solid fa-arrow-trend-up"></i></a>
+    <a href="contact-1.html" class="sw-btn sw-blue-btn wow fadeInUp mt40" data-wow-delay="1.4s">Hire a Developer </a>
     <div class="hero-iconsets wow fadeInUp" data-wow-delay="1.6s">
     <a href="#"><img src="{{ asset('images/icons/goodfirm-1.svg') }}" alt="img"></a>
     <a href="#"><img src="{{ asset('images/icons/clutch-1.svg') }}" alt="img"></a>
@@ -44,12 +44,12 @@
     </div>
     <div class="col-lg-7 mmt40">
     <div class="hero-rght-sw wow fadeInUp" data-wow-delay="1.8s">
-    <div class="video-button"> <a href="https://www.youtube.com/watch?v=Aty3TV4Yn4I" class="video-play"> <span class="play-btn"> <i class="fa fa-play"></i> </span> </a> </div>
+    {{--<div class="video-button"> <a href="https://www.youtube.com/watch?v=Aty3TV4Yn4I" class="video-play"> <span class="play-btn"> <i class="fa fa-play"></i> </span> </a> </div>-- --}}
     <h3>We Design Digital Solutions <span>For Brands, Companies & Startups  </span> to help businesses digitally transform.</h3>
     </div>
     <div class="cardsevc service--cards owl-carousel wow fadeInUp" data-wow-delay="2.5s">
     <div class="service-card-div service-slide">
-    <a href="#">
+    <a href="/services-web">
     <div class="service-images"><img src="{{ asset('images/object/Other-07.png') }}" alt="#"></div>
     <div class="service-name">Web <br> Design </div>
     <div class="circleffect">
@@ -58,7 +58,7 @@
     </a>
     </div>
     <div class="service-card-div service-slide">
-    <a href="#">
+    <a href="/services-app">
     <div class="service-images"><img src="{{ asset('images/object/Other-04.png') }}" alt="#"></div>
     <div class="service-name">App <br> Development </div>
     <div class="circleffect">
@@ -67,16 +67,16 @@
     </a>
     </div>
     <div class="service-card-div service-slide">
-    <a href="#">
+    <a href="/services-desktop">
     <div class="service-images"><img src="{{ asset('images/object/Other-20.png') }}" alt="#"></div>
-    <div class="service-name">Graphic <br> Design </div>
+    <div class="service-name">Desktop<br> Development </div>
     <div class="circleffect">
     <div class="animation">&nbsp;</div>
     </div>
     </a>
     </div>
     <div class="service-card-div service-slide">
-    <a href="#">
+    <a href="/services-web">
     <div class="service-images"><img src="{{ asset('images/object/Other-11.png') }}" alt="#"></div>
     <div class="service-name">Web Application <br> Development </div>
     <div class="circleffect">
@@ -85,7 +85,7 @@
     </a>
     </div>
     <div class="service-card-div service-slide">
-    <a href="#">
+    <a href="/services-web">
     <div class="service-images"><img src="{{ asset('images/object/Other-16.png') }}" alt="#"></div>
     <div class="service-name">Ecommerce <br> Development </div>
     <div class="circleffect">
@@ -94,7 +94,7 @@
     </a>
     </div>
     <div class="service-card-div service-slide">
-    <a href="#">
+    <a href="/">
     <div class="service-images"><img src="{{ asset('images/object/Other-13.png') }}" alt="#"></div>
     <div class="service-name">Digital <br> Marketing </div>
     <div class="circleffect">
@@ -118,7 +118,7 @@
     <h2 class="mb20 wow fadeIn" data-wow-delay=".4s" data-wow-duration="1500ms">The Best Experts of IT & Development Domain For Your Project</h2>
     <p class="wow fadeIn" data-wow-delay=".6s" data-wow-duration="1500ms">Rapid innovation is an essential feature of digital information technology. TechnoPert understands the importance of remaining current through frequent self-reinvention. We accomplish this by continually attracting the brightest minds in modern digital paradigms and platforms.</p>
     <p class="wow fadeIn" data-wow-delay=".8s" data-wow-duration="1500ms">By maintaining a roster of skilled technology professionals across every business function, TechnoPert distinguishes itself by offering its client organizations a single touchpoint to address all of their enterprise technology needs.</p>
-    <a href="contact-1.html" class="sw-btn sw-blue-btn mt30">Read More</a>
+    <a href="/about-us" class="sw-btn sw-blue-btn mt30">Read More</a>
     </div>
     </div>
     <div class="col-lg-5 mmt40">
@@ -306,7 +306,7 @@
     <div class="row">
     <div class="col-lg-6">
     <h2 class="text-gradient-1 wow fadeInUp" data-wow-delay=".4s">Latest Projects</h2>
-    <a href="portfolio.html" class="inline-btn mt20 wow fadeInUp" data-wow-delay=".8s">View all Projects <i class="fa-solid fa-arrow-trend-up"></i></a>
+    <a href="/projects-all" class="inline-btn mt20 wow fadeInUp" data-wow-delay=".8s">View all Projects <i class="fa-solid fa-arrow-trend-up"></i></a>
     </div>
     <div class="col-lg-6 mmt30">
     <p class=" wow fadeInUp" data-wow-delay="1.2s">TechnoPert overcomes challenges, achieves results, and adds value to our clients and partners. Take a look at some of our clients' success stories. Take a look at some of our clients' success stories.</p>
@@ -318,20 +318,20 @@
     <div class="col-lg-12">
     <div class="work-slide owl-carousel full-button centerbtns">
     <div class="our-works">
-    <div class="work-imags"><a href="#"><img src="{{ asset('images/portfolio/work-1.jpg') }}" alt="work" class="card-img-round"></a></div>
+    <div class="work-imags"><a href="https://www.weddme.pk/0"><img src="{{ asset('images/portfolio/weddme.png') }}" alt="work" class="card-img-round"></a></div>
     <div class="infoblocis">
     <div class="nameofitem">
-    <h3>Kyntra: Online Shopping</h3>
-    <p class="mt5">Flutter Development</p>
+    <h3>WeddMe PK: Spouse Finding Site</h3>
+    <p class="mt5">Web Application</p>
     </div>
     </div>
     </div>
     <div class="our-works">
-    <div class="work-imags"><a href="#"><img src="{{ asset('images/portfolio/work-2.jpg') }}" alt="work" class="card-img-round"></a></div>
+    <div class="work-imags"><a href="https://multibranchimsv2.pears.info/"><img src="{{ asset('images/portfolio/multibranchimsv2.pears.info.png') }}" alt="work" class="card-img-round"></a></div>
     <div class="infoblocis">
     <div class="nameofitem">
-    <h3>Bradley's Bakery</h3>
-    <p class="mt5">Web Design</p>
+    <h3>Multibranch School Management App</h3>
+    <p class="mt5">Web Application</p>
     </div>
     </div>
     </div>
@@ -339,35 +339,54 @@
     <div class="work-imags"><a href="#"><img src="{{ asset('images/portfolio/work-3.jpg') }}" alt="work" class="card-img-round"></a></div>
     <div class="infoblocis">
     <div class="nameofitem">
-    <h3>Tango Fashion Store</h3>
+    <h3>Fashion Store</h3>
     <p class="mt5">App Development</p>
     </div>
     </div>
     </div>
     <div class="our-works">
-    <div class="work-imags"><a href="#"><img src="{{ asset('images/portfolio/work-4.jpg') }}" alt="work" class="card-img-round"></a></div>
+    <div class="work-imags"><a href="https://easyprocure.pears.info/"><img src="{{ asset('images/portfolio/easyprocure.pears.info.png') }}" alt="work" class="card-img-round"></a></div>
     <div class="infoblocis">
     <div class="nameofitem">
-    <h3>Ruppay Investment</h3>
-    <p class="mt5">iOs Development</p>
+    <h3>Easyprocure: C2C E-comm</h3>
+    <p class="mt5">Web Application</p>
     </div>
     </div>
     </div>
     <div class="our-works">
-    <div class="work-imags"><a href="#"><img src="{{ asset('images/portfolio/work-5.jpg') }}" alt="work" class="card-img-round"></a></div>
+    <div class="work-imags"><a href="https://ecommerce-store-cms.pears.info/admin"><img src="{{ asset('images/portfolio/ecommerce-store-cms.pears.info.png') }}" alt="work" class="card-img-round"></a></div>
     <div class="infoblocis">
     <div class="nameofitem">
-    <h3>Kiya NFT Market</h3>
+    <h3>E-Commerece Site with CMS Admin Panal</h3>
     <p class="mt5">Web Development</p>
     </div>
     </div>
     </div>
     <div class="our-works">
-    <div class="work-imags"><a href="#"><img src="{{ asset('images/portfolio/work-3.jpg') }}" alt="work" class="card-img-round"></a></div>
+    <div class="work-imags"><a href="https://office.rsfsoft.com/"><img src="{{ asset('images/portfolio/office.rsfsoft.com.png') }}" alt="work" class="card-img-round"></a></div>
     <div class="infoblocis">
     <div class="nameofitem">
-    <h3>Tango Fashion Store</h3>
-    <p class="mt5">App Development</p>
+    <h3>Office Management System</h3>
+    <p class="mt5">Web Development</p>
+    </div>
+    </div>
+    </div>
+    <div class="our-works">
+    <div class="work-imags"><a href="https://movers.runbusinesssmartly.com/"><img src="{{ asset('images/portfolio/movers.runbusinesssmartly.com.png') }}" alt="work" class="card-img-round"></a></div>
+    <div class="infoblocis">
+    <div class="nameofitem">
+    <h3>Transport Management System</h3>
+    <p class="mt5">Web Development</p>
+    </div>
+    </div>
+    </div>
+    
+    <div class="our-works">
+    <div class="work-imags"><a href=""><img src="{{ asset('images/portfolio/small-store-ecom.png') }}" alt="work" class="card-img-round"></a></div>
+    <div class="infoblocis">
+    <div class="nameofitem">
+    <h3>Single Vendor E-commerece Store</h3>
+    <p class="mt5">Web Development</p>
     </div>
     </div>
     </div>
@@ -406,7 +425,7 @@
     <div class="cta-info-div-bkt">
     <h3 class="mb15">Let's Create an Amazing Project Together!</h3>
     <p>Web design app development for Android & iOS. We have over 5 years of experience in helping companies.</p>
-    <a href="#" class="sw-btn sw-white-btn mt30">Get Started <i class="fa-solid fa-arrow-trend-up"></i></a>
+    <a href="/contact-us" class="sw-btn sw-white-btn mt30">Get Started <i class="fa-solid fa-arrow-trend-up"></i></a>
     </div>
     <div class="cta-img-div-bkt"><img src="{{ asset('images/common/app-mockup-2.svg') }}" alt="img"></div>
     </div>
@@ -420,7 +439,7 @@
     <div class="col-lg-6">
     <h3 class="text-gradient-1 mb15 wow fadeIn" data-wow-delay=".2s">Get An Exclusively Brewed Digital Solution For Your Business</h3>
     <p class="wow fadeIn" data-wow-delay=".6s">At TechnoPert, we believe in delivering digital solutions that are meticulously crafted for your business. We understand that every organization has unique goals, challenges, and requirements. That's why we offer exclusively brewed digital solutions tailored to your specific needs. Our team of experts will work closely with you to understand your business inside out and design a customized strategy that aligns perfectly with your objectives.</p>
-    <a href="#" class="sw-btn sw-blue-btn mt30 wow fadeInUp" data-wow-delay="1s">Get Started <i class="fa-solid fa-arrow-trend-up"></i></a>
+    <a href="/contact-us" class="sw-btn sw-blue-btn mt30 wow fadeInUp" data-wow-delay="1s">Get Started <i class="fa-solid fa-arrow-trend-up"></i></a>
     <div class="medianumbers mt40">
     <div class="mediablock wow fadeInUp" data-wow-delay=".8s">
     <div class="ex---">
@@ -448,7 +467,7 @@
     <div class="user-image wh"><img src="{{ asset('images/team/mabidm.jpg') }}" alt="review"></div>
     <div class="user-content">
     <h5>Muhammad Abid Maqbool</h5>
-    <p>CEO at TechnoPert</p>
+    <p>TechnoPert Team Lead</p>
     </div>
     </div>
     </div>
@@ -492,10 +511,6 @@
     <div class="roundiconwithdata wow fadeIn" data-wow-delay="1.6s">
     <div class="imgimg"><img src="{{ asset('images/icons/online-shop.svg') }}" alt="img"> </div>
     <p>eCommerce</p>
-    </div>
-    <div class="roundiconwithdata wow fadeIn" data-wow-delay="1.9s">
-    <div class="imgimg"><img src="{{ asset('images/icons/game-controller.svg') }}" alt="img"> </div>
-    <p>Game</p>
     </div>
     <div class="roundiconwithdata wow fadeIn" data-wow-delay="2.2s">
     <div class="imgimg"><img src="{{ asset('images/icons/healthcare.svg') }}" alt="img"> </div>
@@ -697,13 +712,13 @@
         <div class="contactinfodivsw mt30">
         <div class="contactnumberdiv">
         <p class="mb5 linktitle">We're Available 24/7. Call Now.</p>
-        <a href="#"><i class="fa-solid fa-tty mr10"></i> 91 123 4567 890</a>
-        <a href="#"><i class="fa-brands fa-whatsapp mr10"></i> 91 123 4567 890</a>
+        <a href="tel:+923006916670"><i class="fa-solid fa-tty mr10"></i> 92 300 691 6670</a>
+        <a href="tel:+923069513612"><i class="fa-brands fa-whatsapp mr10"></i> 92 306 951 3612</a>
         </div>
         <div class="contactnumberdiv mt30">
         <p class="mb5 linktitle">Send Us an Email:</p>
-        <a href="#"><i class="fa-solid fa-envelope mr10"></i> <span class="__cf_email__" data-cfemail="127a60526b7d6760657770617b66773c717d7f">[email&#160;protected]</span></a>
-        <a href="#"><i class="fa-solid fa-envelope mr10"></i> <span class="__cf_email__" data-cfemail="4f262129200f36203a3d382a2d3c263b2a612c2022">[email&#160;protected]</span></a>
+        <a href="mailto:abid@technopert.com"><span class="__cf_email__" data-cfemail="ec85828a83ac9583999e9b898e9f859889c28f8381">abid@technopert.com</span></a>
+        <a href="mailto:abidmaqbool20@gmail.com"><span class="__cf_email__" data-cfemail="ec85828a83ac9583999e9b898e9f859889c28f8381">abidmaqbool20@gmail.com</span></a>
         </div>
         <div class="contactnumberdiv mt30">
         <p class="mb5 linktitle">Chat on Skype:</p>
@@ -827,14 +842,11 @@
     <div class="footer-links">
     <h5 class="linktitle">Company Links</h5>
     <ul class="mt20">
-    <li><a href="#">About Us</a></li>
-    <li><a href="#">Contact Us</a></li>
-    <li><a href="#">Careers</a></li>
-    <li><a href="#">Our Team</a></li>
-    <li><a href="#">Media Coverage</a></li>
-    <li><a href="#">Referral Program</a></li>
-    <li><a href="#">Case Studies</a></li>
-    <li><a href="#">Client Testimonials</a></li>
+    <li><a href="/about-us">About Us</a></li>
+    <li><a href="/contact-us">Contact Us</a></li>
+    <li><a href="/">Careers</a></li>
+    <li><a href="/portfolio-team">Our Team</a></li>
+    <li><a href="/portfolio-ceo">Team Lead</a></li>
     </ul>
     </div>
     </div>
@@ -842,14 +854,14 @@
     <div class="footer-links">
     <h5 class="linktitle">Technologies</h5>
     <ul class="mt20">
-    <li><a href="#">React JS</a></li>
-    <li><a href="#">Laravel</a></li>
-    <li><a href="#">CodeIgniter</a></li>
-    <li><a href="#">Node JS</a></li>
-    <li><a href="#">WordPress</a></li>
-    <li><a href="#">Magento</a></li>
-    <li><a href="#">ReactJS</a></li>
-    <li><a href="#">KnockoutJs</a></li>
+    <li><a href="">React JS</a></li>
+    <li><a href="">Laravel</a></li>
+    <li><a href="">CodeIgniter</a></li>
+    <li><a href="">Node JS</a></li>
+    <li><a href="">WordPress</a></li>
+    <li><a href="">React Native</a></li>
+    <li><a href="">Window form</a></li>
+    <li><a href="">ElectronJs</a></li>
     </ul>
     </div>
     </div>
@@ -857,14 +869,10 @@
     <div class="footer-links">
     <h5 class="linktitle">Our Services</h5>
     <ul class="mt20">
-    <li><a href="#">Web Application</a></li>
-    <li><a href="#">Mobile App Development</a></li>
-    <li><a href="#">Microsoft Development</a></li>
-    <li><a href="#">Front End Development</a></li>
-    <li><a href="#">eCommerce Development</a></li>
-    <li><a href="#">Cross-platform App</a></li>
-    <li><a href="#">Opensource Development</a></li>
-    <li><a href="#">UI/UX Design</a></li>
+    <li><a href="/services-web">Web Application</a></li>
+    <li><a href="/services-app">Mobile App Development</a></li>
+    <li><a href="/services-web">eCommerce Development</a></li>
+    <li><a href="/services-desktop">Cross-platform Desktop App</a></li>
     </ul>
     </div>
     </div>
